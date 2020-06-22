8697c7b38dfffb6c8f35557cff0bc85064cc880d
Rahul Isaac <37054264+rahulisaac@users.noreply.github.com>
2020-06-22 12:13:25 -0400

    Fixed doors and added config.txt
    Doors are now bumped out to 2d rectangles instead lines of so you can open & close them in FGU. 
    You can edit an optional config.txt file to edit input_path, output_path and the width of doors. 
    See readme.txt for details.

--- draft2fgu.py
+++ draft2fgu.py
@@ -1,39 +1,56 @@
-from json import load
+from json import load, loads
 from base64 import decodebytes
 from xml.etree.ElementTree import Element, tostring
-from os import listdir
+from os import listdir, getcwd, path
 from os.path import isfile, join
+from math import sin, cos
 
-def convert_to_fgu(filename):
-    with open(f'{filename}.dd2vtt') as f:
+def convert_to_fgu(filename, input_path, output_path, portal_width):  
+    with open(join(input_path, f'{filename}.dd2vtt')) as f:
         file = load(f)
         
     ppg = file['resolution']['pixels_per_grid']
     x_dim = file['resolution']['map_size']['x']
     y_dim = file['resolution']['map_size']['y']
 
+    if portal_width[-1] == '%':
+        epsilon = max(ppg*float(portal_width[:-1])/100,1)/2
+    elif portal_width[-2:] == 'px':
+        epsilon = float(portal_width[:-2])/2
+
     occular_list = list()
 
     for figure in file['line_of_sight']:
         wall_dict = dict()
+        wall_dict['type'] = 'wall'
+        
         wall_dict['x'] = [coord['x'] for coord in figure]
         wall_dict['y'] = [coord['y'] for coord in figure]
-        
+
         occular_list.append(wall_dict)
         
     for door in file['portals']:
         door_dict = dict()
-        
+
+        door_dict['type'] = 'door'
+
         door_dict['x'] = [coord['x'] for coord in door['bounds']]
         door_dict['y'] = [coord['y'] for coord in door['bounds']]
         
-        door_dict['closed'] = door['closed'] 
+        door_dict['closed'] = door['closed']
+        door_dict['rotation'] = door['rotation']
+
         occular_list.append(door_dict)
 
     for occular in occular_list:
         x_trans = [x*ppg - (x_dim*ppg)//2 for x in occular['x']]
         y_trans = [-y*ppg + (y_dim*ppg)//2 for y in occular['y']]
         
+        if occular['type'] == 'door':
+            epsilon_x = epsilon * sin(occular['rotation'])
+            epsilon_y = epsilon * cos(occular['rotation'])
+            x_trans = [x_trans[0]-epsilon_x, x_trans[1]-epsilon_x, x_trans[1]+epsilon_x, x_trans[0]+epsilon_x]
+            y_trans = [y_trans[0]-epsilon_y, y_trans[1]-epsilon_y, y_trans[1]+epsilon_y, y_trans[0]+epsilon_y]
         occular['coords'] = [a for item in zip(x_trans, y_trans) for a in item]
         
     root = Element('root')
@@ -51,7 +68,7 @@ def convert_to_fgu(filename):
         points.text = ','.join(str(o) for o in object_['coords'])
         occluder.append(points)
 
-        if 'closed' in object_:
+        if object_['type'] == 'door':
             door = Element('door')
             door.text = 'true'
             occluder.append(door)
@@ -62,14 +79,28 @@ def convert_to_fgu(filename):
             
         occluders.append(occluder)
 
-    with open(f'{filename}.xml', 'wb') as f:
+    with open(join(output_path,f'{filename}.xml'), 'wb') as f:
         f.write(tostring(root))
         
 
-    with open(f'{filename}.png','wb') as f:
+    with open(join(output_path,f'{filename}.png'),'wb') as f:
         f.write(decodebytes(file['image'].encode('utf-8')))
 
+input_path=getcwd()
+output_path=getcwd()
+portal_width='25%'
 
-dd2vtt_files = [f[:-7] for f in listdir() if f.endswith(".dd2vtt") ]
+if path.exists('config.txt'):
+    with open('config.txt') as f:
+        config = loads(f.read().replace('\\','/'))
+
+    if config['input_path'] != '':
+        input_path = config['input_path']
+    if config['output_path'] != '':
+        output_path = config['output_path']
+    if config['portal_width'] != '':
+        portal_width=config['portal_width']
+
+dd2vtt_files = [f[:-7] for f in listdir(input_path) if f.endswith('.dd2vtt')]
 for filename in dd2vtt_files:
-    convert_to_fgu(filename)
+    convert_to_fgu(filename,input_path,output_path,portal_width)
---
+++ readme.txt
@@ -0,0 +1,59 @@
+How to use this script:
+    1. Export your Dungoendraft map in the .dd2vtt format
+    2. Place the script/executable in the same folder as your .dd2vtt file (or files)
+    3. Run the script (just double click on the executable)
+    4. Voila! Your .png & .xml files have been created and are in the same folder. It will do this for all .dd2vtt files in the folder. 
+	Place these in your "Fantasy Grounds\campaigns\{campaign name}\images" folder and import in FGU as normal.
+
+If you want to modify the default behavior, you can modify the parameters in the "config.txt" file:
+
+input_path: 
+	* The location of your .dd2vtt files
+	* The default value of empty quotes will read from the folder the script is run from
+	* It can take any path to a FOLDER, for example
+	  "C:/Users/my_user_name/Pictures/my_dungeon_draft_files"
+
+output_path
+	* The location you want your .png and .xml files placed
+	* You may choose to place these directly in your Fantasy Grounds folder, for example
+	  "C:/Users/Adam/AppData/Roaming/SmiteWorks/Fantasy Grounds/campaigns/OneShot/image"
+	* The default value of empty quotes will write to the folder the script is run from
+	* PLEASE NOTE THAT THIS SCRIPT WILL OVERWRITE EXISITING FILES WITH THE SAME NAME & EXTENSION WITHOUT ASKING
+
+portal_width
+	* The depth of all doors in your map
+	* This can be defined as a percent of a grid square or as an absolute pixel value
+	* For example, if your grid has 200px and you specify "20%", your doors will have a depth of 40px
+	* Decimals are fine. For example, if you want a width of 1/8th of a square, specify "12.5%" 
+	* Alternaively, instead of a percent, you can specify an absolute value. If you want it to be exactly 50 pixels, specify "50px"
+	* Empty quotes "" will result in a default value of 25%
+
+The script makes no attempt to correct bad parameters, so please format them exactly as written in the instructions. 
+If the file "config.txt" is not present in the same directory, the default values are use.
+Below are some valid examples of what you could put in the "config.txt" file 
+
+YOU WILL NEED TO MODIFY FILE PATHS TO POIN TO VALID LOCATIONS ON YOUR COMPUTER
+
+1.
+
+{
+	"input_path":"C:\Users\my_user_name\Documents\dnd",
+	"output_path":"C:\Users\my_user_name\AppData\Roaming\SmiteWorks\Fantasy Grounds\campaigns\OneShot\images",
+	"portal_width":"12.5%"
+}
+
+2.
+
+{
+	"",
+	"",
+	"portal_width":"40px"
+}
+
+3.
+
+{
+	"input_path":"",
+	"output_path":"C:\Users\my_user_name\AppData\Roaming\SmiteWorks\Fantasy Grounds\campaigns\OneShot\images",
+	"portal_width":""
+}
